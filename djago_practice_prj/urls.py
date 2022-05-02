443916f25d55d7af811b73e951c62c55bf6ccf48
이나경 <66313756+nakyoung98@users.noreply.github.com>
2022-05-02 19:18:00 +0900

    장고 프로젝트 생성

---
+++ djago_practice_prj/urls.py
@@ -0,0 +1,21 @@
+"""djago_practice_prj URL Configuration
+사용자가 어떤 URL 형식으로 접근했을 떄 어떻게 웹사이트를 작동시킬지를 정리해놓은 파일
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
