0a633f860d3a0f95588b13de26163a8e7166ff8a
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2022-05-03 22:42:03 +0900

    settings에 blog, single_pages 앱 추가

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField(max_length= 1000)
+
+    created_at = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.title}'
+
+
